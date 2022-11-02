e2fa32961094a858c65386099891276e57bbe169
Ivan Pronin <ivanp@playtika.com>
2022-11-02 20:58:47 +0300

    Remove copyrights

--- src/test/java/com/codewars/fundamental/StringRepeat.java
+++ src/test/java/com/codewars/fundamental/StringRepeat.java
@@ -1,8 +1,3 @@
-/*
- * Copyrights 2020 Playtika Ltd., all rights reserved to Playtika Ltd.
- * privacy+e17bb14d-edc1-4d26-930d-486fcc1ab8fe@playtika.com
- */
-
 package com.codewars.fundamental;
 
 import java.util.stream.Collectors;
